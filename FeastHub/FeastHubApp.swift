9f43b47701719caeacaefc50c6f81c230b21112b
kevin.jonathan <kevin.jonathan@gdn-commerce.com>
2022-12-10 19:59:31 +0700

    add starter files

---
+++ FeastHub/FeastHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeastHubApp.swift
+//  FeastHub
+//
+//  Created by Kevin Jonathan on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FeastHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
