572bec166b386aad7ff395411c4f638b4faa4b3a
kevin.jonathan <kevin.jonathan@gdn-commerce.com>
2022-12-25 18:16:57 +0700

    Readd test in Restaurant Package

---
+++ FeastHub/Restaurant/Tests/RestaurantTests/RestaurantTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import Restaurant
+
+final class RestaurantTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+}
